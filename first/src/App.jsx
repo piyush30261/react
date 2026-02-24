8e7403eeee93767fbb3c01e750f2cc7e670a5de3
piyush30261 <piyushkumar638634@gmail.com>
2026-02-24 07:38:54 +0000

    mest

---
+++ first/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+  <h1> First react project</h1>
+  <Employee></Employee>
+  </div>
+  )
+
+  function Employee(){
+    
+
+    
+  return(
+      <h1>Piyush</h1>
+    )
+
+  }
+
+
+
+}
+export default App
